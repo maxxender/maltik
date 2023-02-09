01ac309ebc30059af33eccb96b2768449e9c1a86
Matinou Badarou <badaroumatinou@gmail.com>
2023-02-09 09:17:31 +0100

    set password profil

--- php/set-password.php
+++ php/set-password.php
@@ -1,33 +1,54 @@
 <?php
 session_start();
+$ajax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+$errors = [];
 
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $error = [];
     if(empty($_POST['recentPass']) || empty($_POST['newPass'])){
-        $error['error_empty_field_pass'] = 'Formulaire non remplis dans son intégralité';
-        echo json_encode($error);
+        $errors['error_empty_field_pass'] = 'Certains champs du formulaire sont vides. Veuillez réessayez';
     }
     if($_POST['recentPass'] == $_POST['newPass']){
         
-        $error['error_set_pass'] =  'Mot de passe Nouveau et ancien fifférent. Réessayez';
-        echo json_encode($error);
+        $errors[] =  'Votre ancien Mot de Passe et votre nouveau Mot de Passe sont similaire. Veuillez les changez';
     }
-    include_once 'pdo.php';
-    $recentPass = htmlspecialchars($_POST['recentPass']);
-    $newPass = htmlspecialchars($_POST['newPass']);
-    
-    $req = $pdo->prepare('SELECT mdp FROM students WHERE id = ?');
-    $req->execute(array(
-        $_SESSION['id_user']
-    ));
-    $hash = $datas['mdp'] = $req->fetch();
-    if(password_verify($recentPass, $hash)){
-        $setPass = $pdo->prepare('UPDATE students SET mdp = ? WHERE id = ?');
-        $setPass->execute((array(
-            password_hash($newPass, PASSWORD_BCRYPT),
+    if(strlen(($_POST['newPass'])) < 8){
+        $errors[] = 'Votre mot de passe est trop court! Il doit comporté au moins 08 caractères';
+    }
+    if(ctype_alpha($_POST['newPass'])){
+        $errors[] = 'Votre Nouveau mot de passe doit contenu au moins un chiffre';
+    }
+
+    if(empty($errors)){
+        include_once 'pdo.php';
+        $recentPass = htmlspecialchars($_POST['recentPass']);
+        $newPass = htmlspecialchars($_POST['newPass']);
+        $req = $pdo->prepare('SELECT mdp FROM students WHERE id = ?');
+        $req->execute(array(
             $_SESSION['id_user']
+        ));
+        $datas = $req->fetch();
+        $hash = $datas['mdp'];
+        if(password_verify($recentPass, $hash)){
+            $setPass = $pdo->prepare('UPDATE students SET mdp = ? WHERE id = ?');
+            $setPass->execute((array(
+                password_hash($newPass, PASSWORD_BCRYPT),
+                $_SESSION['id_user']
         )));
+
+            if($ajax){
+                echo json_encode(['Votre mot de passe a été mis à jour']);
+                http_response_code(200);
+            }else{
+                header('location:./dashboard.php#set-profil');
+            }
+
+        }else{
+            if($ajax){
+                echo json_encode(['Votre mot de passe a été mis à jour']);
+                http_response_code(400);
+            }else{
+    
+            }
+        }
+    }else{
+
     }
-}else{
-    echo "pas de javascript";
-}
