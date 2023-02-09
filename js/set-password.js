1802018d67f47ca4ead8374dffca4c7ade5683fd
Matinou Badarou <badaroumatinou@gmail.com>
2023-02-09 09:18:24 +0100

    add setpassowrd.js

--- js/set-password.js
+++ js/set-password.js
@@ -1,21 +1,53 @@
-(function(){
-    var formPass = document.querySelector('#formSetPassword')
-    formPass.addEventListener('submit',function(e){
-        e.preventDefault();
-        var formdata = new FormData(formPass);
-        var xhr = new XMLHttpRequest();
+function validatePass(elePass){
+    if(elePass.value.length < 4){
+        return false;
+    }
+    return true;
+}
 
-        xhr.onreadystatechange = function(e){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    console.log(JSON.parse(xhr.responseText));
-                }
+function ajaxPost(adresse, form){
+    var xhr = new XMLHttpRequest();
+    var formdata = new FormData(form);
+    xhr.onreadystatechange = function(e){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                return JSON.parse(xhr.responseText);
             }
         }
+    }
+    xhr.open('POST', adresse, true);
+    xhr.send(formdata);
+}
 
-        xhr.open('POST', formPass.getAttribute('action'), true);
-        xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
-        xhr.send(formdata);
+(function(){
+    var formPass = document.querySelector('#formSetPassword');
+    var eleRecentPass = document.querySelector('#recentPass');
+    var eleNewPass = document.querySelector('#newPass');
+    var eleSubmitForm = formPass.querySelector('button');
+    var errorsRecentPass = [];
+    const errorsNewPass = [];
+
+  
+
+    formPass.addEventListener('submit',function(e){
+        e.preventDefault();
+            console.log("submit clique")
+            var formdata = new FormData(formPass);
+            var xhr = new XMLHttpRequest();
+    
+            xhr.onreadystatechange = function(e){
+                if(xhr.readyState === 4){
+                    if(xhr.status === 200){
+                        console.log(JSON.parse(xhr.responseText));
+                    }else{
+                        console.log(JSON.parse(xhr.responseText));
+                    }
+                }
+            }
+    
+            xhr.open('POST', formPass.getAttribute('action'), true);
+            xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+            xhr.send(formdata);
 
     });
 })();
